a110d2b85769c3ff281f63b918c030ebbc4889eb
alicegillard <alicegillard.ed@gmail.com>
2019-02-15 16:55:02 +0100

    - WIP - burger/tweenmax test

--- assets/app/index.js
+++ assets/app/index.js
@@ -1,8 +1,12 @@
 var controller = new ScrollMagic.Controller();
 
+var plan1 = document.getElementById("plan1"),
+    trigger1 = document.getElementById("trigger1"),
+    tween1 = TweenMax.to(plan1, 2, {opacity:0.5});
+
 var scene = new ScrollMagic.Scene(
-  {triggerElement:"#trigger"}
+  {triggerElement:trigger1, duration:100}
 )
-.setTween('#plan1', 1, {y:200})
+.setTween(tween1)
 .addIndicators({name:"start"})
 .addTo(controller);
