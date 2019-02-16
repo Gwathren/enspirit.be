7c751160401b7dd8de4e6073a8b50fa88765a941
Bernard Lambeau <blambeau@gmail.com>
2019-02-16 10:14:50 +0100

    Fix javascript.

--- assets/app/index.js
+++ assets/app/index.js
@@ -1,31 +1,24 @@
-/* test 1 -- fonction dans html */
-function changeMainnavClass() {
-  var NAME = document.getElementsByClassName("mainnav-tablet")
-  NAME.className="mainnav-tablet-open"
-}
+$(function(){
 
-/* test 2 -- addEventListener */
-document.getElementById("clickMe").addEventListener("click", function(){
-  document.getElementsByClassName("mainnav-tablet").toggleClass('mainnav-tablet-open');
+  /* test 3 -- jQuery */
+  var clickMe = document.getElementById("clickMe");
+  $(clickMe).click(function() {
+    $('.mainnav-tablet').toggleClass('mainnav-tablet-open');
+  });
+
+
+  /* test 4 -- ScrollMagic */
+  var controller = new ScrollMagic.Controller();
+
+  var plan1 = document.getElementById("plan1"),
+      trigger1 = document.getElementById("trigger1"),
+      tween1 = TweenMax.to(plan1, 2, {opacity:0.5});
+
+  var scene = new ScrollMagic.Scene(
+    {triggerElement:trigger1, duration:100}
+  )
+  .setTween(tween1)
+  .addIndicators({name:"start"})
+  .addTo(controller);
+
 });
-
-/* test 3 -- jQuery */
-var clickMe = document.getElementById("clickMe");
-$(clickMe).click(function() {
-  $('.mainnav-tablet').toggleClass('.mainnav-tablet-open');
-});
-
-
-/* test 4 -- ScrollMagic */
-var controller = new ScrollMagic.Controller();
-
-var plan1 = document.getElementById("plan1"),
-    trigger1 = document.getElementById("trigger1"),
-    tween1 = TweenMax.to(plan1, 2, {opacity:0.5});
-
-var scene = new ScrollMagic.Scene(
-  {triggerElement:trigger1, duration:100}
-)
-.setTween(tween1)
-.addIndicators({name:"start"})
-.addTo(controller);
