ffbaa3cd766bb0d9d8e1f9dd954390f6cb46f766
alicegillard <alicegillard.ed@gmail.com>
2019-02-21 17:19:08 +0100

    - WIP -
    * parralax (home/vision)
    * animation menu

--- assets/app/index.js
+++ assets/app/index.js
@@ -3,22 +3,89 @@ $(function(){
   /* test 3 -- jQuery */
   var clickMe = document.getElementById("clickMe");
   $(clickMe).click(function() {
-    $('.mainnav-tablet').toggleClass('mainnav-tablet-open');
+    $('.span').toggleClass('span-unvisible'),
+    $('.menu').toggleClass('menu-open'),
+    $('.nav-overlayer').toggleClass('nav-overlayer-open');
   });
 
 
-  /* test 4 -- ScrollMagic */
-  var controller = new ScrollMagic.Controller();
+  var mql = window.matchMedia("screen and (min-width: 768px)")
+  if (mql.matches){
+   /*alert("Window is 768px or wider")*/
 
-  var plan1 = document.getElementById("plan1"),
-      trigger1 = document.getElementById("trigger1"),
-      tween1 = TweenMax.to(plan1, 2, {opacity:0.5});
+    /* plan -2 */
+    var controller1 = new ScrollMagic.Controller();
+    var scene1 = new ScrollMagic.Scene({
+       offset: 0,
+       duration:600
+     })
+    .setTween("#animate-2", {y:30})
+    /*.addIndicators({name:"-2"})*/
+    .addTo(controller1);
 
-  var scene = new ScrollMagic.Scene(
-    {triggerElement:trigger1, duration:100}
-  )
-  .setTween(tween1)
-  .addIndicators({name:"start"})
-  .addTo(controller);
+    /* plan -1 */
+    var controller2 = new ScrollMagic.Controller();
+    var scene2 = new ScrollMagic.Scene({
+       offset: 0,
+       duration:600
+     })
+    .setTween("#animate-1", {y:15})
+    /*.addIndicators({name:"-1"})*/
+    .addTo(controller2);
+
+    /* plan 1 */
+    var controller3 = new ScrollMagic.Controller();
+    var scene3 = new ScrollMagic.Scene({
+       offset: 0,
+       duration:600
+     })
+    .setTween("#animate1", {y:0})
+    /*.addIndicators({name:"1"})*/
+    .addTo(controller3);
+
+    /* plan 2 */
+    var controller4 = new ScrollMagic.Controller();
+    var scene4 = new ScrollMagic.Scene({
+       offset: 0,
+       duration:600
+     })
+    .setTween("#animate2", {y:50})
+    /*.addIndicators({name:"2"})*/
+    .addTo(controller4);
+
+    /* plan 3 */
+    var controller5 = new ScrollMagic.Controller();
+    var scene5 = new ScrollMagic.Scene({
+      offset: 0,
+      duration:600
+    })
+    .setTween("#animate3", {y:40})
+    /*.addIndicators({name:"3"})*/
+    .addTo(controller5);
+
+    /* plan 4 */
+    var controller6 = new ScrollMagic.Controller();
+    var scene6 = new ScrollMagic.Scene({
+      offset: 0,
+      duration:600
+    })
+    .setTween("#animate4", {y:30})
+    /*.addIndicators({name:"4"})*/
+    .addTo(controller6);
+
+    /* plan 5 */
+    var controller7 = new ScrollMagic.Controller();
+    var scene7 = new ScrollMagic.Scene({
+      offset: 0,
+      duration:600
+    })
+    .setTween("#animate4", {y:15})
+    /*.addIndicators({name:"4"})*/
+    .addTo(controller7);
+
+  }
+  else{
+   // do something else
+  }
 
 });
